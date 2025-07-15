474fd5409275bcbe4f480e6d0dcf80d07fe8b41f
Tran Dinh Nam <nam.tran@bnksolution.com>
2025-07-15 14:22:30 +0700

    add debug function for env var

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './router';
 import { UserProvider } from './context/UserContext';
+
+// Hàm in ra các biến môi trường, có thể gọi từ console
+function printEnvVars() {
+  console.log(import.meta.env);
+}
+(window as any).printEnvVars = printEnvVars;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
